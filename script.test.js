b105d45f2081f66341c4fadb116f92efbf774470
gabfedora <gabrieldarosa.poll@gmail.com>
2026-07-26 21:10:25 -0300

    create a function that reverse a string and test it

--- script.test.js
+++ script.test.js
@@ -1,5 +1,9 @@
-import capitalize from './script.js'
+import {capitalize, reverseString} from './script.js'
 
 test('capitalize something', () => {
     expect(capitalize('hello')).toBe('Hello')
 });
+
+test('take a string and reverse it', () => {
+    expect(reverseString('gabriel')).toBe('leirbag');
+})
